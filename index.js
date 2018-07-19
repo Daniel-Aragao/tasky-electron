88ae34fbbd30d1fb2cf7dc944a537efbbd9ff0d8
Daniel Aragão <danielfilhoce@gmail.com>
2018-07-18 22:03:13 -0300

    aula 46

--- index.js
+++ index.js
@@ -4,22 +4,42 @@ const electron = require('electron');
 const { app , BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         height: 500,
         width: 300,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false
     });
+
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-    if(process.platform === 'linux'){
-        process.env.XDG_CURRENT_DESKTOP = 'Unity';
-    }
+    tray = new Tray(iconPath);
 
-    new Tray(iconPath);
+    tray.on('click', function(event, bounds){
+
+        const {x, y} = bounds;
+        const {height, width} = mainWindow.getBounds();
+
+        if(mainWindow.isVisible()){
+            mainWindow.hide();
+        }else{
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height: height,
+                width: width 
+            });
+
+            mainWindow.show();
+        }
+    });
 });
